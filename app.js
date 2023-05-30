93bc62cc5980d5b2b8afebda73c02093c72bbcfa
mahfuzur <mdmahfuazur7788@gamil.com>
2023-05-30 16:52:19 +0600

    13.2 Setup App For EJS

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+//Setup view Engine
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// Middleware Array
+
+const middleware = [
+  morgan("dev"),
+  express.static("public"),
+  express.urlencoded({ extended: true }),
+  express.json(),
+];
+
+app.use(middleware);
+
+
 
 app.get("/", (req, res) => {
   res.json({
     message: "Hello Programmer",
   });
 });
+
 
 const PORT = process.env.PORT || 9000;
 
