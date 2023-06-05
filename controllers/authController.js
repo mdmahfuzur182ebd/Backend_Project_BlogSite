bbc7d92e1856d2655216d0b5af7291c8c23b0fe4
mahfuzur <mdmahfuazur7788@gamil.com>
2023-06-05 10:12:30 +0600

    login controller user and email bcrypt

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,13 +14,14 @@ exports.signupPostController = async (req, res, next) => {
     //hash password
     let hashPassword = await bcrypt.hash(password, 11);
 
-    let user = new User({ //create user
+    let user = new User({
+      //create user
       username,
       email,
       password: hashPassword,
     });
 
-    let createUser = await user.save();  //user save or store data
+    let createUser = await user.save(); //user save or store data
     console.log("User Created Successfully", createUser);
     res.render("pages/auth/signup", { title: "Crate A New Account" });
 
@@ -32,12 +33,35 @@ exports.signupPostController = async (req, res, next) => {
 
 exports.loginGetController = (req, res, next) => {
 
-   res.render('pages/auth/login', {title:"Login to Your Account"})
+  res.render("pages/auth/login", { title: "Login to Your Account" });
 
 };
 
-exports.loginPostController = (req, res, next) => {
-  
+exports.loginPostController = async (req, res, next) => {
+  let { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if(!user){
+      return res.json({
+        message:"Invalid Credential"
+      })
+    }
+
+    let match = await bcrypt.compare(password, user.password);
+    if(!match){
+      return res.json({
+        message: "Invalid Credential",
+      });
+    }
+
+    console.log('Successfully Logged In ', user)
+    res.render("pages/auth/login", { title: "Login to Your Account" });
+
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
 };
 
 exports.logoutController = (req, res, next) => {};
