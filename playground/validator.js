ab74d25b197f4e6586ee1880b7d348210507f4e3
mahfuzur <mdmahfuazur7788@gamil.com>
2023-06-05 15:32:06 +0600

    Validation Check with Error Message

--- playground/validator.js
+++ playground/validator.js
@@ -1,12 +1,27 @@
 const router = require("express").Router();
-
-const { check } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 
 router.get("/validator", (req, res, next) => {
-  
   res.render("playground/signup", { title: "validator playground" });
 });
 
-router.post("/validator", [check("username")], (req, res, next) => {});
+router.post(
+  "/validator",
+  [
+    check("username")
+      .not()
+      .isEmpty().withMessage('Username Can not empty')
+      .isLength({ max: 15 })
+      .withMessage(`Username can not be greater than 15 Character`),
+
+    check("email").isEmail().withMessage("Please Provide A Valid Email"),
+  ],
+
+  (req, res, next) => {
+    let errors = validationResult(req);
+    console.log(errors);
+    res.render("playground/signup", { title: "validator playground" });
+  }
+);
 
 module.exports = router;
