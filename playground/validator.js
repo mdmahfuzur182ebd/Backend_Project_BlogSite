617d2ccb1d0f526472435dc8db5d0cb0b3d486dd
mahfuzur <mdmahfuazur7788@gamil.com>
2023-06-05 17:27:57 +0600

    Custom validation

--- playground/validator.js
+++ playground/validator.js
@@ -10,17 +10,31 @@ router.post(
   [
     check("username")
       .not()
-      .isEmpty().withMessage('Username Can not empty')
+      .isEmpty()
+      .withMessage("Username Can not empty")
       .isLength({ max: 15 })
       .withMessage(`Username can not be greater than 15 Character`),
 
     check("email").isEmail().withMessage("Please Provide A Valid Email"),
+
+    check("password").custom((value) => {
+      if (value.length < 5) {
+        throw new Error("Password Must be Greater than 5 Character");
+      }
+      return true;
+    }),
+    check("confirmPassword").custom((value, { req }) => {
+      if (value != req.body.password) {
+        throw new Error("Password Does Not Match");
+      }
+      return true;
+    }),
   ],
 
   (req, res, next) => {
     let errors = validationResult(req);
 
-    const formatter = (error) => error.msg
+    const formatter = (error) => error.msg;
 
     // console.log(errors.isEmpty());
     // console.log(errors.array());
