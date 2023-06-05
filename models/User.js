74798ef31142ee710d50215a47a8cad8db2b7fea
mahfuzur <mdmahfuazur7788@gamil.com>
2023-06-05 17:37:39 +0600

    Sanitizer

--- models/User.js
+++ models/User.js
@@ -14,7 +14,6 @@ const userSchema = new Schema(
     },
     email: {
       type: String,
-      trim: true,
       required: true,
     },
     password: {
--- playground/validator.js
+++ playground/validator.js
@@ -13,9 +13,13 @@ router.post(
       .isEmpty()
       .withMessage("Username Can not empty")
       .isLength({ max: 15 })
-      .withMessage(`Username can not be greater than 15 Character`),
+      .withMessage(`Username can not be greater than 15 Character`)
+      .trim(),
 
-    check("email").isEmail().withMessage("Please Provide A Valid Email"),
+    check("email")
+      .isEmail()
+      .withMessage("Please Provide A Valid Email")
+      .normalizeEmail(),
 
     check("password").custom((value) => {
       if (value.length < 5) {
@@ -23,6 +27,7 @@ router.post(
       }
       return true;
     }),
+
     check("confirmPassword").custom((value, { req }) => {
       if (value != req.body.password) {
         throw new Error("Password Does Not Match");
@@ -40,6 +45,7 @@ router.post(
     // console.log(errors.array());
     // console.log(errors.mapped());
     console.log(errors.formatWith(formatter).mapped());
+    console.log(req.body.username, req.body.email);
 
     res.render("playground/signup", { title: "validator playground" });
   }
